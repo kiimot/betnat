5cdbe6cafbf1b51bb1ccbddce8ace4c27cf5fbeb
kiimot <kimlopezmoreno@gmail.com>
2022-03-03 14:34:40 +0100

    Avances 03-03-22

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
     <head>
-        <title>HotPizza</title>
+        <title>BETNAT</title>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="./assets/css/style.css" type="text/css"/>
@@ -11,13 +11,28 @@
         <!-- codigo web principal -->
         <!-- recepcion de datos -->
         <?php
-            if (isset($_POST['evento'])){
-                echo 'Has seleccionado '. $_POST['evento'];
+        session_start();
+        //Creamos la variable de sesion si no existe
+        $i = 0;
+        if (!isset($_SESSION['eventos']) && !isset($_SESSION['seleccionEventos'])){
+            $_SESSION['eventos'] = [] ;
+            $_SESSION['seleccionEventos'] = [] ; 
+        }
+        else{
+            // Miramos si nos ha llegado una pizza seleccionada por parametro
+            if (isset($_POST['evento']) && isset($_POST['seleccionEvento'])){
+                $eventoSeleccionado = $listaEventos[$_POST['evento']];
+                $seleccionEvento = $_POST['seleccionEvento'];
+                //Añadimos la pizza a la variable sesion
+                $_SESSION['eventos'][]= $eventoSeleccionado;
+                $i = count($_SESSION['seleccionEventos']);
+                $_SESSION['seleccionEventos'][$i]= $seleccionEvento;
             }
+        }
+
         ?>
 
         <br><br>
-
         <table>
             <tr>
                 <th>Código Evento</th>
@@ -37,35 +52,103 @@
                     <td><?= $evento->getFechaEvento(); ?></td>
                     <td>
                         <?php if (($evento->getTipoEvento() == "Futbol") || ($evento->getTipoEvento() == "Basquet")){ ?>
-                            <?= $evento->getLogoEquipoLocal(); ?> <br> <?= $evento->getLogoEquipoVisitante(); ?>
+                            <img style="width:35px;height:35px;" src="./assets/img/<?= $evento->getLogoEquipoLocal(); ?>"><br>
+                            <img style="width:35px;height:35px;" src="./assets/img/<?= $evento->getLogoEquipoVisitante(); ?>">
                         <?php } else { ?>
-                            <?= $evento->getNacionalidadEquipoLocal(); ?> <br> <?= $evento->getNacionalidadEquipoVisitante(); ?>
+                            <img style="width:35px;height:25px;" src="./assets/img/<?= $evento->getNacionalidadEquipoLocal(); ?>"><br><br>
+                            <img style="width:35px;height:25px;" src="./assets/img/<?= $evento->getNacionalidadEquipoVisitante(); ?>">
                         <?php } ?>
                     </td>
                     <td><?= $evento->getNombreEquipoLocal(); ?> <br> <?= $evento->getNombreEquipoVisitante(); ?></td>
                     <td>
                         <form action='./index.php' method='post'>
-                            <input type="hidden" name="evento" value=<?= $evento->getCuotaEquipoLocal();?> >
+                            <input type="hidden" name="evento" value=<?= $evento->getCodigoEvento();?> >
+                            <input type="hidden" name="seleccionEvento" value=1>
                             <input type="submit" value="<?= $evento->getCuotaEquipoLocal(); ?>">
                         </form> 
                     </td>
                     <td>
                         <?php if ($evento->getTipoEvento() == "Futbol"){?>
                         <form action='./index.php' method='post'>
-                            <input type="hidden" name="evento" value=<?= $evento->getCuotaEmpate();?> >
+                            <input type="hidden" name="evento" value=<?= $evento->getCodigoEvento();?> >
+                            <input type="hidden" name="seleccionEvento" value="X">
                             <input type="submit" value="<?= $evento->getCuotaEmpate(); ?>">
                         </form> 
                         <?php } ?>
                     </td>                   
                     <td>
                         <form action='./index.php' method='post'>
-                            <input type="hidden" name="evento" value=<?= $evento->getCuotaEquipoVisitante();?> >
+                            <input type="hidden" name="evento" value=<?= $evento->getCodigoEvento();?> >
+                            <input type="hidden" name="seleccionEvento" value=2>
                             <input type="submit" value="<?= $evento->getCuotaEquipoVisitante(); ?>">
                         </form> 
                     </td>
                 </tr>
             <?php } ?>
         </table>
+
+
+
+        <br><br>
+
+
+
+
+        <h2>APUESTAS SELECCIONADAS</h2>
+        <?php
+        if (!empty($_SESSION['eventos']) && !empty($_SESSION['seleccionEventos'])){?>
+            <?php  foreach ($_SESSION['seleccionEventos'] as $seleccionEvento ){
+                    echo "$seleccionEvento";
+                }
+                $contador = -1;
+            ?>
+            
+            <table>
+            <tr>
+                <th>Fecha</th>
+                <th></th>
+                <th>Partido</th>
+                <th>Selección</th>
+                <th>Cuota</th>
+                <th>Cantidad</th>
+            </tr>
+            <?php
+            foreach ($_SESSION['eventos'] as $evento ){ ?>
+                <tr>
+                    <td><?= $evento->getFechaEvento(); ?></td>
+                    <td>
+                        <?php if (($evento->getTipoEvento() == "Futbol") || ($evento->getTipoEvento() == "Basquet")){ ?>
+                            <img style="width:35px;height:35px;" src="./assets/img/<?= $evento->getLogoEquipoLocal(); ?>"><br>
+                            <img style="width:35px;height:35px;" src="./assets/img/<?= $evento->getLogoEquipoVisitante(); ?>">
+                        <?php } else { ?>
+                            <img style="width:35px;height:25px;" src="./assets/img/<?= $evento->getNacionalidadEquipoLocal(); ?>"><br><br>
+                            <img style="width:35px;height:25px;" src="./assets/img/<?= $evento->getNacionalidadEquipoVisitante(); ?>">
+                        <?php } ?>
+                    </td>
+                    <td><?= $evento->getNombreEquipoLocal(); ?> <br> <?= $evento->getNombreEquipoVisitante(); ?></td>
+                    <?php $contador++; ?>
+                    <?php
+                        if ($_SESSION['seleccionEventos'][$contador] == 1){?><td><?= $evento->getNombreEquipoLocal(); ?></td><?php }
+                        elseif ($_SESSION['seleccionEventos'][$contador] == "X"){?><td>Empate</td><?php }
+                        elseif ($_SESSION['seleccionEventos'][$contador] == 2){?><td><?= $evento->getNombreEquipoVisitante(); ?></td><?php }
+                    ?>
+                    <?php
+                        if ($_SESSION['seleccionEventos'][$contador] == 1){?><td><?= $evento->getCuotaEquipoLocal(); ?></td><?php }
+                        elseif ($_SESSION['seleccionEventos'][$contador] == "X"){?><td><?= $evento->getCuotaEmpate(); ?></td><?php }
+                        elseif ($_SESSION['seleccionEventos'][$contador] == 2){?><td><?= $evento->getCuotaEquipoVisitante(); ?></td><?php }
+                    ?>
+                    <td>
+                        <input type="number" name="cantidad">
+                        <form action='./index.php' method='post'>
+                            <input type="hidden" name="evento" value=<?= $evento->getCodigoEvento();?> >
+                            <input class="bet" type="submit" value="BET">
+                            <input class="del" type="submit" value="DEL">
+                        </form>
+                    </td>
+                </tr>
+            <?php } ?>
+        </table>
+        <?php } ?>
         <!-- include footer -->
     </body>
 </html>
--- views/cabecera.php
+++ views/cabecera.php
@@ -2,17 +2,20 @@
 
 #Crearemos eventos de fútbol, básquet y tenis
 include './modelo/eventoDeportivo.php';
+//logoLiverpool = new Image ( );
+//logoLiverpool.src = "./assets/img/liverpool.png";
+//$f1="./assets/img/liverpool.jpg";
 
-$liverpoolVSmadrid = new Futbol("F01","Futbol","26/06/2022 21:00","Liverpool","Real Madrid","imagen","imagen",1.60,2.30,3.10);
-$chelseaVSbarcelona = new Futbol("F02","Futbol","27/06/2022 21:00","Chelsea","FC Barcelona","imagen","imagen",2.60,3.30,2.10);
+$liverpoolVSmadrid = new Futbol("F01","Futbol","26/06/2022 21:00","Liverpool","Real Madrid","liverpool.png","madrid.png",1.60,2.30,3.10);
+$chelseaVSbarcelona = new Futbol("F02","Futbol","27/06/2022 21:00","Chelsea","FC Barcelona","chelsea.png","barcelona.png",2.60,3.30,2.10);
 
-$lakersVSceltics = new Basquet("B01","Basquet","13/04/2022 04:00","Los Angeles Lakers","Boston Celtics","imagen","imagen",2.50,1.35);
-$warriorsVSclippers = new Basquet("B02","Basquet","16/04/2022 01:00","Golden State Warriors","Los Angeles Clippers","imagen","imagen",1.70,1.95);
+$lakersVSceltics = new Basquet("B01","Basquet","13/04/2022 04:00","Los Angeles Lakers","Boston Celtics","lakers.png","celtics.png",2.50,1.35);
+$warriorsVSclippers = new Basquet("B02","Basquet","16/04/2022 01:00","Golden State Warriors","Los Angeles Clippers","warriors.png","clippers.png",1.70,1.95);
 
-$medevedevVSnadal = new Tenis("T01","Tenis","02/05/2022 05:10","Daniil Medevev","Rafael Nadal","imagen","imagen",1.66,2.20);
-$djokovicVSalcaraz = new Tenis("T02","Tenis","30/04/2022 07:00","Novak Djokovic","Carlos Alcaraz","imagen","imagen",1.20,3.80);
+$medevedevVSnadal = new Tenis("T01","Tenis","02/05/2022 05:10","Daniil Medevev","Rafael Nadal","rusia.png","españa.png",1.66,2.20);
+$djokovicVSalcaraz = new Tenis("T02","Tenis","30/04/2022 07:00","Novak Djokovic","Alexander Zverev","serbia.png","alemania.png",1.20,3.80);
 
 
-$listaEventos = array($liverpoolVSmadrid,$chelseaVSbarcelona,$lakersVSceltics,$warriorsVSclippers,$medevedevVSnadal,$djokovicVSalcaraz);
+$listaEventos = array("F01" => $liverpoolVSmadrid, "F02" => $chelseaVSbarcelona, "B01" => $lakersVSceltics, "B02" => $warriorsVSclippers, "T01" => $medevedevVSnadal, "T02" => $djokovicVSalcaraz);
 
 ?>
